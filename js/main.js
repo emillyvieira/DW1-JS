a35353e4943486cbf0329d588be7b7decbf849a7
emillyvieira <emilly.vieira190505@gmail.com>
2023-10-09 22:15:06 -0300

    js

--- js/main.js
+++ js/main.js
@@ -68,3 +68,68 @@ function adicionaProduto() {
     document.getElementById('imagem-produto').value = '';
   }
   
+// anonima
+
+// const adicionaProduto = () => {
+//   const nomeProduto = document.getElementById('nome-produto').value || 'Nome';
+//   const precoProduto = document.getElementById('preco-produto').value || 'XX,xx';
+//   const imagemProduto = document.getElementById('imagem-produto').value || 'https://static.vecteezy.com/ti/vetor-gratis/p1/5006046-enviar-presente-cartao-referencia-codigo-conceito-ilustracao-plano-design-eps10-moderno-elemento-grafico-para-pagina-de-destino-vazio-estado-ui-infografico-icone-vetor.jpg';
+
+//   const cardNovo = `
+//     <div class="card horizontal">
+//       <img src="${imagemProduto}" alt="produto" width="198px">
+//       <div>
+//         <h3>${nomeProduto}</h3>
+//         <p>R$${precoProduto}</p>
+//       </div>
+//     </div>
+//   `;
+
+//   const container = document.getElementById('container');
+//   container.innerHTML += cardNovo;
+
+//   document.getElementById('nome-produto').value = '';
+//   document.getElementById('preco-produto').value = '';
+//   document.getElementById('imagem-produto').value = '';
+// }
+
+
+
+const testeFuncAnonima = function() {
+  console.log('função anonima')
+}
+
+testeFuncAnonima()
+
+const funcArrow = () => {
+  console.log('funcao arrow')
+}
+
+funcArrow()
+
+const soma = (value1, value2) => {
+  const resultado = value1 + value2
+  return resultado
+}
+
+const soma10 = value1 => console.log(value1 + 10)
+
+soma10(5)
+
+let res = soma(20, 10)
+
+const var1 = 10
+const var2 = 8 
+
+// res = soma(var1, var2)
+
+// console.log(parseInt())
+
+const calculadora = (operador) => {
+
+
+
+
+
+
+}
